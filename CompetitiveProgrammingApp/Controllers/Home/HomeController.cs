09b99716853ec5ca479857098994addb0bde5d7a
Alethor <oscarbralo@outlook.com>
2015-08-25 00:28:28 +0200

    First commit, initial app skeleton

---
+++ CompetitiveProgrammingApp/Controllers/Home/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CompetitiveProgrammingApp.Models;
+
+namespace CompetitiveProgrammingApp.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetIntoHackerrank()
+        {
+            HomeModel homeModel = new HomeModel();
+            homeModel.HackerRankSuccess = true;
+            return Json(homeModel);
+        }
+
+    }
+}
---
+++ CompetitiveProgrammingApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompetitiveProgrammingApp.MvcApplication" Language="C#" %>
